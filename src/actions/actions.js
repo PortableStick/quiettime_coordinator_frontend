58187c3268cbb00b04626294dca780665955858f
Gregory Smith <crashingwaves@fea.st>
2017-01-02 21:33:30 -0700

    Add redux stuff

---
+++ src/actions/actions.js
@@ -0,0 +1,11 @@
+// geolocate
+// set search data
+// get yelp results
+// receive yelp results
+// patch update attending
+// post create user
+// post login
+// receive user data
+// logout
+// post password reset
+// receive password reset
